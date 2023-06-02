8b01f37fc01185d0f85f676afb0643b33d6f1e4c
이원희 <story3798@naver.com>
2023-06-02 22:11:01 +0900

    second

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'dart:js_interop';
+
 import 'package:flutter/material.dart';
 import 'style.dart' as style;
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(
@@ -20,6 +24,20 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
 
   var tab = 0;
+  var date = [];
+
+  getData() async{
+    var result = await http.get(Uri.parse('https://codingapple1.github.io/app/data.json'));
+    var result2 = jsonDecode(result.body);
+    setState(() {
+      date = result2;
+    });
+  }
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,120 +52,15 @@ class _MyAppState extends State<MyApp> {
           )
         ],
       ),
-      body: ListView(
-        children: [
-          SizedBox(
-            width: 100,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                    height: 510,
-                    width: double.infinity,
-                    child: Image.asset('octopus.jpg',scale: 1,),
-                ),
-                SizedBox(
-                  height: 100,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        '     좋아요 100',
-                        style: TextStyle(
-                            fontSize: 15,
-                            color: Colors.black,
-                            fontWeight: FontWeight.w700),
-                      ),
-                      Text(
-                        '     글쓴이',
-                        style: TextStyle(
-                            fontSize: 15,
-                            color: Colors.black,),
-                      ),
-                      Text(
-                        '     글내용',
-                        style: TextStyle(
-                            fontSize: 15,
-                            color: Colors.black,),
-                      ),
-                    ],
-                  )
-                ),
-                SizedBox(
-                  height: 510,
-                  width: double.infinity,
-                  child: Image.asset('octopus.jpg',scale: 1,),
-                ),
-                SizedBox(
-                    height: 100,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          '     좋아요 100',
-                          style: TextStyle(
-                              fontSize: 15,
-                              color: Colors.black,
-                              fontWeight: FontWeight.w700),
-                        ),
-                        Text(
-                          '     글쓴이',
-                          style: TextStyle(
-                            fontSize: 15,
-                            color: Colors.black,),
-                        ),
-                        Text(
-                          '     글내용',
-                          style: TextStyle(
-                            fontSize: 15,
-                            color: Colors.black,),
-                        ),
-                      ],
-                    )
-                ),
-                SizedBox(
-                  height: 510,
-                  width: double.infinity,
-                  child: Image.asset('octopus.jpg',scale: 1,),
-                ),
-                SizedBox(
-                    height: 100,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          '     좋아요 100',
-                          style: TextStyle(
-                              fontSize: 15,
-                              color: Colors.black,
-                              fontWeight: FontWeight.w700),
-                        ),
-                        Text(
-                          '     글쓴이',
-                          style: TextStyle(
-                            fontSize: 15,
-                            color: Colors.black,),
-                        ),
-                        Text(
-                          '     글내용',
-                          style: TextStyle(
-                            fontSize: 15,
-                            color: Colors.black,),
-                        ),
-                      ],
-                    )
-                ),
-              ],
-            ),
-          )
-        ],
-      ),
+      body: [Home(data : date), Text('샵페이지')][tab],
       bottomNavigationBar: BottomNavigationBar(
         showUnselectedLabels: false,
         showSelectedLabels: false,
+        onTap: (i){
+          setState(() {
+            tab = i;
+          });
+        },
         items: [
           BottomNavigationBarItem(
               label : '홈',
@@ -162,5 +75,31 @@ class _MyAppState extends State<MyApp> {
         ],
       ),
     );
+  }
+}
+
+
+class Home extends StatelessWidget {
+  const Home({super.key,this.data});
+
+  final data;
+
+  @override
+  Widget build(BuildContext context) {
+    if(data != null){
+      return ListView.builder(itemBuilder: (c,i){
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.network(data[i]['image']),
+            Text('좋아요 ${data[i]['likes']}'),
+            Text('${data[i]['user']}'),
+            Text('${data[i]['content']}'),
+          ],
+        );
+      },itemCount: 3,);
+    }else{
+      return Text('로딩중임');
+    }
   }
 }
